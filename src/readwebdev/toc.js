ca45b4fa707747dce5d4d7642694aa728e5be9cb
Andy Shu Xin <andy@shux.in>
2016-03-29 22:20:52 +0800

    Implement click to expand

--- src/readwebdev/toc.js
+++ src/readwebdev/toc.js
@@ -1,13 +1,14 @@
 (function(window, document) {
   'use strict'
-  const headings = document.querySelectorAll('h2, h3, h4')
-  for (let heading of [].slice.apply(headings)) {
-    heading.addEventListener('click', (event) => {
-      let sibiling = heading.nextElementSibling
-      while (sibiling) {
-        sibiling.classList.toggle('display')
-        sibiling = sibiling.nextElementSibling
-      }
-    })
+
+  function activate(event) {
+    let sibiling = event.target.nextElementSibling
+    event.target.parentNode.classList.toggle('active')
+    while (sibiling) {
+      sibiling.classList.toggle('display')
+      sibiling = sibiling.nextElementSibling
+    }
   }
+  document.body.addEventListener('click', activate)
+
 })(window, window.document)
