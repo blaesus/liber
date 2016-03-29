640cc47d1bcf96c300266c2a45559093b337591f
Andy Shu Xin <andy@shux.in>
2016-03-29 22:30:09 +0800

    Update 2016-03-29T14:29:54.042Z

---
+++ readwebdev/toc.js
@@ -0,0 +1,16 @@
+(function(window, document) {
+  'use strict'
+
+  function activate(event) {
+    if (event.target.nodeName.toLowerCase() === 'h4') {
+      let sibiling = event.target.nextElementSibling
+      event.target.parentNode.classList.toggle('active')
+      while (sibiling) {
+        sibiling.classList.toggle('display')
+        sibiling = sibiling.nextElementSibling
+      }
+    }
+  }
+  document.body.addEventListener('click', activate)
+
+})(window, window.document)
