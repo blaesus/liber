9813b19e84a2ca2caf91c3a846e5161bf11179f1
Andy Shu Xin <andy@shux.in>
2016-05-21 17:37:38 +0800

    Refactor

--- src/readwebdev/toc.js
+++ src/readwebdev/toc.js
@@ -1,12 +1,7 @@
 (function(window, document) {
   function activate(event) {
     if (event.target.nodeName.toLowerCase() === 'h4') {
-      let sibling = event.target.nextElementSibling
       event.target.parentNode.classList.toggle('active')
-      while (sibling) {
-        sibling.classList.toggle('display')
-        sibling = sibling.nextElementSibling
-      }
     }
   }
   document.body.addEventListener('click', activate)
