a343769b2e5fee0992e10f273205f99b43f9c5b3
Andy Shu Xin <andy@shux.in>
2016-05-21 15:17:12 +0800

    Correct typo

--- src/readwebdev/toc.js
+++ src/readwebdev/toc.js
@@ -1,11 +1,11 @@
 (function(window, document) {
   function activate(event) {
     if (event.target.nodeName.toLowerCase() === 'h4') {
-      let sibiling = event.target.nextElementSibling
+      let sibling = event.target.nextElementSibling
       event.target.parentNode.classList.toggle('active')
-      while (sibiling) {
-        sibiling.classList.toggle('display')
-        sibiling = sibiling.nextElementSibling
+      while (sibling) {
+        sibling.classList.toggle('display')
+        sibling = sibling.nextElementSibling
       }
     }
   }
