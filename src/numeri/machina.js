8f039221ffd00987bd8a4ad7d133345161cb303e
Andy Shu <andyshuhsin@gmail.com>
2017-06-19 11:04:05 -0400

    Implement subtractive logic

--- src/numeri/machina.js
+++ src/numeri/machina.js
@@ -10,26 +10,32 @@ function quidquidIntegerNumerus(initium, finis) {
 function novamExercitiaFacere() {
     verbaDOM.value = ''
     indicium.innerHTML = ''
-    numerusDOM.innerHTML = quidquidIntegerNumerus(10, 20)
+    numerusDOM.innerHTML = quidquidIntegerNumerus(1, 100)
 }
 
 function verbumAbNumero(numerus, data) {
     if (numerus <= 10) {
-        return data['1'][numerus - 1]
+        return [data['1'][numerus - 1]]
     }
     else if (numerus <= 20) {
-        return data['10+'][numerus - 1 - 10]
+        return [data['10+'][numerus - 1 - 10]]
     }
     else if (numerus % 10 === 0 && numerus < 100) {
-        return data['*10'][numerus / 10 - 1]
+        return [data['*10'][numerus / 10 - 1]]
+    }
+    else if (numerus % 10 === 8 && numerus < 100 - 2) {
+        return ['duodē' + verbumAbNumero(numerus + 2, data)]
+    }
+    else if (numerus % 10 === 9 && numerus < 100 - 1) {
+        return ['ūndē' + verbumAbNumero(numerus + 1, data)]
     }
     else if (numerus % 100 === 0 && numerus <= 1000) {
-        return data['*100'][numerus / 100 - 1]
+        return [data['*100'][numerus / 100 - 1]]
     }
 }
 
 function aequine(numerus, verba, configuratio) {
-    return verbumAbNumero(numerus, exercitia.data) === verba
+    return verbumAbNumero(numerus, exercitia.data).indexOf(verba) > -1
 }
 
 bullaVerumne.onclick = function() {
