b9089fb64ce5b95fa7081e038446cdb7eb41320a
Andy Shu <andyshuhsin@gmail.com>
2017-06-19 11:50:25 -0400

    Implement compound numeral under 2000

--- src/numeri/machina.js
+++ src/numeri/machina.js
@@ -10,41 +10,55 @@ function quidquidIntegerNumerus(initium, finis) {
 function novamExercitiaFacere() {
     verbaDOM.value = ''
     indicium.innerHTML = ''
-    numerusDOM.innerHTML = quidquidIntegerNumerus(1, 100)
+    numerusDOM.innerHTML = quidquidIntegerNumerus(100, 1000)
+}
+
+// partem(12345, 1)    ===  40
+// partem(12345, 1, 2) === 340
+function partem(numerus, minimum, maximum) {
+    if (typeof maximum === 'undefined') {
+        maximum = minimum + 1
+    }
+    return (numerus % Math.pow(10, maximum)) - (numerus % Math.pow(10, minimum))
 }
 
 function verbumAbNumero(numerus, data) {
     if (numerus <= 10) {
         // ut 8
-        return [data['1'][numerus - 1]]
+        return data['1'][numerus - 1]
     }
     else if (numerus <= 20) {
         // ut 18
-        return [data['10+'][numerus - 1 - 10]]
+        return data['10+'][numerus - 1 - 10]
     }
     else if (numerus % 10 === 0 && numerus < 100) {
         // ut 70
-        return [data['*10'][numerus / 10 - 1]]
+        return data['*10'][numerus / 10 - 1]
     }
     else if (numerus % 10 === 8 && numerus < 100 - 2) {
-        // ut 88, non 98
-        return ['duodē' + verbumAbNumero(numerus + 2, data)]
+        // ut 88, non 88
+        return 'duodē' + verbumAbNumero(numerus + 2, data)
     }
     else if (numerus % 10 === 9 && numerus < 100 - 1) {
         // ut 39, non 99
-        return ['ūndē' + verbumAbNumero(numerus + 1, data)]
+        return 'ūndē' + verbumAbNumero(numerus + 1, data)
     }
     else if (numerus % 100 === 0 && numerus <= 1000) {
         // ut 700
-        return [data['*100'][numerus / 100 - 1]]
+        return data['*100'][numerus / 100 - 1]
     }
-    else {
+    else if (numerus < 2000) {
         // Numerus compostus
+        const digiti = String(numerus).length
+        return [
+            verbumAbNumero(partem(numerus, digiti - 1), data),
+            verbumAbNumero(partem(numerus, 0, digiti - 1), data),
+        ].join(' ')
     }
 }
 
 function aequine(numerus, verba, configuratio) {
-    return verbumAbNumero(numerus, exercitia.data).indexOf(verba) > -1
+    return verbumAbNumero(numerus, exercitia.data) === verba
 }
 
 bullaVerumne.onclick = function() {
