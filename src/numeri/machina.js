b2fe75c57580f026de2eab38f38c7a8ab70423e2
Andy Shu <andyshuhsin@gmail.com>
2017-06-19 11:08:29 -0400

    Document branches

--- src/numeri/machina.js
+++ src/numeri/machina.js
@@ -15,22 +15,31 @@ function novamExercitiaFacere() {
 
 function verbumAbNumero(numerus, data) {
     if (numerus <= 10) {
+        // ut 8
         return [data['1'][numerus - 1]]
     }
     else if (numerus <= 20) {
+        // ut 18
         return [data['10+'][numerus - 1 - 10]]
     }
     else if (numerus % 10 === 0 && numerus < 100) {
+        // ut 70
         return [data['*10'][numerus / 10 - 1]]
     }
     else if (numerus % 10 === 8 && numerus < 100 - 2) {
+        // ut 88, non 98
         return ['duodē' + verbumAbNumero(numerus + 2, data)]
     }
     else if (numerus % 10 === 9 && numerus < 100 - 1) {
+        // ut 39, non 99
         return ['ūndē' + verbumAbNumero(numerus + 1, data)]
     }
     else if (numerus % 100 === 0 && numerus <= 1000) {
+        // ut 700
         return [data['*100'][numerus / 100 - 1]]
+    }
+    else {
+        // Numerus compostus
     }
 }
 
