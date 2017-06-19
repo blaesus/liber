301cfbdfcbf75baf41f5220e1fb9d61a9986a8c7
Andy Shu <andyshuhsin@gmail.com>
2017-06-19 10:27:01 -0400

    Add basic verification logic flow

--- src/numeri/machina.js
+++ src/numeri/machina.js
@@ -1,13 +1,33 @@
-var verba = document.querySelector('.verba')
-var numerus = document.querySelector('.numerus')
+var verbaDOM = document.querySelector('.verba')
+var numerusDOM = document.querySelector('.numerus')
+var bullaVerumne = document.querySelector('.bulla.verumne')
+var indicium = document.querySelector('.indicium')
 
 function quidquidIntegerNumerus(initium, finis) {
     return Math.floor(Math.random() * (finis - initium + 1)) + initium;
 }
 
 function novamExercitiaFacere() {
-    verba.value = ''
-    numerus.innerHTML = quidquidIntegerNumerus(1, 1000)
+    verbaDOM.value = ''
+    indicium.innerHTML = ''
+    numerusDOM.innerHTML = quidquidIntegerNumerus(1, 1000)
+}
+
+function aequine(numerus, verba, configuratio) {
+    return false
+}
+
+bullaVerumne.onclick = function() {
+    if (aequine(numerusDOM.innerHTML, verbaDOM.value)) {
+        indicium.innerHTML = 'Vērum!'
+    }
+    else {
+        indicium.innerHTML = 'Nōn vērum est. Cōnāre iterum?'
+    }
+}
+
+verbaDOM.oninput = function() {
+    console.info('hi')
 }
 
 novamExercitiaFacere()
