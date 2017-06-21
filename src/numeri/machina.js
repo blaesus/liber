0edb397c5f72c468c0cbacf46773ae06eed3880a
Andy Shu <andyshuhsin@gmail.com>
2017-06-21 14:04:01 -0400

    Implement different number formats

--- src/numeri/machina.js
+++ src/numeri/machina.js
@@ -23,6 +23,33 @@ function quidquidIntegerNumerus(initium, finis) {
     return Math.floor(Math.random() * (finis - initium + 1)) + initium;
 }
 
+function arabicoRomanicum(arabicus) {
+    const tabula = {
+        M : 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    }
+    let romanicus = ""
+
+    for (let digitus in tabula) {
+        while (arabicus >= tabula[digitus]) {
+            romanicus += digitus
+            arabicus -= tabula[digitus]
+        }
+    }
+    return romanicus
+}
+
 // partem(12345, 1)    ===  40
 // partem(12345, 1, 2) === 340
 function partem(numerus, minimum, maximum) {
@@ -115,12 +142,14 @@ function numerumPingere() {
     switch (status.ab) {
         case 'arabicus': {
             dom.numerus.innerHTML = status.numerus
-            console.info(dom.numerus.innerHTML)
             break
         }
         case 'litterae': {
             dom.numerus.innerHTML = verbumAbNumero(status.numerus)
-            console.info(dom.numerus.innerHTML)
+            break
+        }
+        case 'romanicus': {
+            dom.numerus.innerHTML = arabicoRomanicum(status.numerus)
             break
         }
     }
