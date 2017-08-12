f6667bcf254b164e143897777bd8fd87406205c4
Andy Shu <andy@symbol.is>
2017-08-12 15:15:07 -0700

    Add favicon

---
+++ src/shared/makefav.sh
@@ -0,0 +1,8 @@
+  convert favicon.svg -transparent white -resize 256x256 \
+          -define icon:auto-resize="64,48,32,16" \
+          ../favicon.ico
+
+  convert favicon.svg -transparent white -resize 152x152 \
+          ../favicon-152.png
+
+
